4c000b919a67eb6a83814f4bfb0286e05ad1eca5
Todd0554 <toddy9229@outlook.com>
2022-10-07 16:37:16 +1100

    home page with products

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import products from '../products'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  return (
+    <>
+        <h1>New Products</h1>
+        <Row>{ products.map(product => {
+            return (
+                <Col sm={12} md={6} lg={3}>
+                    <Product product={product} />
+                </Col>
+            )
+        })}
+        </Row>
+    </>
+  )
+}
+
+export default HomeScreen
